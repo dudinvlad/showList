5bbd08e9c1efa8ea6540462629480021033fd9ea
kalmap <dudin1.vlad@gmail.com>
2019-08-03 09:05:36 +0300

    Initial Commit

---
+++ ShowList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowList
+//
+//  Created by Влад Дудин on 8/3/19.
+//  Copyright © 2019 Vlad Dudin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
